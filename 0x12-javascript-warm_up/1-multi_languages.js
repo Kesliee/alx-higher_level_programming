e0190a58363004b6b5ff6075b7d1619705db27b2
Kesliee <leslieneemakeza@gmail.com>
2024-03-11 11:41:48 -0700

    popss

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(thirdLine);
